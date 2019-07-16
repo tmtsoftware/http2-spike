41895c9bddafab81014c7c07ae54f60e22762ba8
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-07-16 15:44:21 +0530

    added route for websocket

--- akka-http-metrics/src/main/scala/com/example/example.scala
+++ akka-http-metrics/src/main/scala/com/example/example.scala
@@ -4,10 +4,14 @@ import java.time.LocalTime
 import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
 import java.util.{Timer, TimerTask}
 
+import akka.actor.Cancellable
 import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
 import akka.http.scaladsl.model.sse.ServerSentEvent
+import akka.http.scaladsl.model.ws.TextMessage
 import akka.http.scaladsl.server.{Directives, Route}
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.{Done, NotUsed}
+import com.example.parameter
 
 import scala.concurrent.duration.{DurationLong, FiniteDuration}
 import scala.concurrent.{Future, Promise}
@@ -31,9 +35,33 @@ package object example extends Directives {
 
   import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
+  def printSink[T]: Sink[T, Future[Done]] = Sink.foreach(println)
+
+  def randomNumberSource(
+    duration: Option[FiniteDuration]
+  ): Source[TextMessage.Strict, Cancellable] = {
+    val source = Source
+      .tick(0.second, 500.millis, 0)
+      .map(_ => Random.nextInt(100))
+      .map(number => TextMessage(number.toString))
+
+    duration match {
+      case Some(value) => source.takeWithin(value)
+      case None        => source
+    }
+  }
+
   val allRoutes: Route = cors() {
     get {
-      path("sse") {
+      (path("websocket") & parameter('durationInSeconds.as[Int] ?)) {
+        durationInSeconds =>
+          handleWebSocketMessages(
+            Flow.fromSinkAndSourceCoupled(
+              printSink,
+              randomNumberSource(durationInSeconds.map(x => x.seconds))
+            )
+          )
+      } ~ path("sse") {
         parameter('durationInSeconds.as[Int] ?) { durationInSeconds =>
           complete {
             val s = Source
